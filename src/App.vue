c515110dc5534c64494ef7e666d9c72ca2325281
mzz <2831376148@qq.com>
2023-06-30 20:10:59 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,51 @@
 <template>
-  <div>
-    <my-button></my-button>
-    <h1>我是demo01</h1>
+  <div class="box">
+    <h1>我是根组件</h1>
+    <!-- 子组件 -->
+    <!-- 加上冒号可以解析 -->
+    <!-- <my-goods name="mzz" :price="30000" id="66"></my-goods> -->
+    <!-- <my-goods id="77"></my-goods> -->
+
+    <my-goods v-for="item in list" :key="item.id" 
+    :name="item.name" 
+    :price="item.price" 
+    :id="item.id"
+    @price="doPrice"></my-goods>
+    <!-- 这里不用接收参数，因为是默认事件对象 -->
   </div>
 </template>
 
 <script>
-export default {
+// 注册组件
+import MyGoods from '@/components/MyGoods.vue'
 
+export default {
+  components: {
+    MyGoods
+  },
+  data() {
+    return {
+      list: [
+        { name: '苹果手机', price: 5888, id: 123 },
+        { name: '小米手机', price: 1888, id: 456 },
+        { name: '华为手机', price: 3666, id: 789 },
+      ]
+    }
+  },
+  methods: {
+    doPrice(id) {
+      console.log(id)
+      // 根据id找到哪一件商品
+      const item = this.list.find(item => item.id === id)
+      // 修改商品价格
+      item.price --;
+    }
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.box {
+  border: 2px solid purple;
+}
+</style>
---
+++ src/components/MyGoods.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="box">
+    <p>商品名称：{{ name }}</p>
+    <p>商品价格：{{ price }}</p>
+    <p>商品编号：{{ id }}</p>
+    <button @click="doClick">点我砍一刀</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyGoods',
+  // 父传子 当成子组件中的data即可，直接用
+  //props: ["属性名"] 数组写法，没有开启校验
+  //props: { 属性名：校验规则 } 对象写法，开启校验
+  // props: ['name','price','id'],
+  props: {
+    name: String,
+    price: {
+      // 给前面父组件传来的属性前加 :
+      type: Number,
+      default: 100
+    },
+    id: {
+      type: [Number, String],
+      required: true
+    }
+  },
+  methods: {
+    doClick() {
+      // 子传父：自定义事件
+      // this.$emit('自定义事件名',事件对象（参数）)
+      // 这里传过来的就是事件对象默认参数$event 写谁传谁
+      this.$emit('price', this.id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* */
+.box {
+  border: 2px solid purple;
+}
+</style>
