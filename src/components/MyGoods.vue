2cb422a3009357af9095c3eb66c5dc4dd6ebee6f
mzz <2831376148@qq.com>
2023-06-30 16:55:47 +0800

    update

---
+++ src/components/MyGoods.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="box">
+        <h2>商品名称：</h2>
+        <h2>商品价格：</h2>
+        <h2>商品编号：</h2>
+        <button>点我砍一刀</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.box {
+    border: 2px solid purple;
+}
+</style>
